cbefc7e8a27d7f2ef6ef5f06d7987c0e4c6cf6d9
DEV01 VALIANT <vgdev@VG-DEV01>
2023-09-20 00:22:43 -0300

    feat: style component comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 const Post = () => {
@@ -33,6 +34,21 @@ const Post = () => {
         <p>
           <a href="">#novoprojeto #nlw #rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
