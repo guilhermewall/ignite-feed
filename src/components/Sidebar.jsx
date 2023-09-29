bd551b4de67601158aa53682992182af70d5149e
DEV01 VALIANT <vgdev@VG-DEV01>
2023-09-29 00:14:50 -0300

    feat: add functional componet Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import Avatar from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "phosphor-react";
 
@@ -11,11 +12,7 @@ export const Sidebar = () => {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/guilhermewall.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/guilhermewall.png" />
         <strong>Guilherme Wallace</strong>
         <span>Web Developer</span>
       </div>
